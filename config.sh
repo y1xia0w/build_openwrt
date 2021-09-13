bec2bd18403ccf17bf5db70d22cf7bebe51506ae
Yixiao <50680028+y1xia0w@users.noreply.github.com>
2021-09-13 10:30:52 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Zero/g' package/base-files/files/bin/config_generate
+
 git clone https://github.com/vernesong/OpenClash ../OpenClash && ln -s ../../OpenClash/luci-app-openclash package/
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
