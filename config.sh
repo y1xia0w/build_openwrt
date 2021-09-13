9a235f4a8c6d3b4a7d3c4290e042832c4d46f186
Yixiao <50680028+y1xia0w@users.noreply.github.com>
2021-09-12 23:41:35 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,1 +1,3 @@
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/vernesong/OpenClash ../OpenClash && ln -s ../../OpenClash/luci-app-openclash package/
+echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
