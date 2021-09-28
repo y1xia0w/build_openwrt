7bbe122a39d4d173b1f09c6312f5733f410dedf2
Yixiao <50680028+y1xia0w@users.noreply.github.com>
2021-09-27 22:12:12 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@ sed -i 's/OpenWrt/Zero/g' package/base-files/files/bin/config_generate
 git clone https://github.com/vernesong/OpenClash ../OpenClash && ln -s ../../OpenClash/luci-app-openclash package/
 echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 
-git clone https://github.com/small-5/luci-app-adblock-plus ../ && ln -s ../luci-app-adblock-plus package/
+git clone https://github.com/small-5/luci-app-adblock-plus ../luci-app-adblock-plus && ln -s ../luci-app-adblock-plus package/
 echo "CONFIG_PACKAGE_luci-app-adblock-plus=y" >> .config
 
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome ../luci-app-adguardhome && ln -s ../../luci-app-adguardhome package/
